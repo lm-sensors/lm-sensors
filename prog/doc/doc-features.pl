174f451f7aaf13cb7a598b78520b5ca91bf7bfcf
Frodo Looijaard <frodol@dds.nl>
1999-04-14 21:06:31 +0000

    Final slight changes to the documentation generators.
    lm75 and lm78 documentation files are now up-to-date.
    
    
    git-svn-id: http://lm-sensors.org/svn/lm-sensors/trunk@392 7894878c-1315-0410-8ee3-d5d059ff63e0

--- prog/doc/doc-features.pl
+++ prog/doc/doc-features.pl
@@ -296,8 +296,8 @@ sub main {
   initialize;
   my ($base_dir,@prefixes) = scan_arguments @_;
   scan_all $base_dir;
-  print "Module Parameters\n";
-  print "-----------------\n\n";
+  print "Chip Features\n";
+  print "-------------\n\n";
   if (@prefixes) {
     foreach $prefix (@prefixes) {
       foreach $el (keys %features) {
