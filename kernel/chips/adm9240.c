084ee510ab83807db6c1cf25b55f35b60b91e558
Frodo Looijaard <frodol@dds.nl>
1999-04-20 15:33:04 +0000

    ADM9240/DS1780 documentation and library support
    * Readings for the ADM9240 are now updated every 0.5 seconds; for the
      DS1780, every 2.0 seconds.
    * I still think it likely that voltage readings will be far-off in the
      current implementation
    
    
    git-svn-id: http://lm-sensors.org/svn/lm-sensors/trunk@401 7894878c-1315-0410-8ee3-d5d059ff63e0

--- kernel/chips/adm9240.c
+++ kernel/chips/adm9240.c
@@ -549,7 +549,7 @@ void adm9240_update_client(struct i2c_client *client)
 
   down(&data->update_lock);
 
-  if ((jiffies - data->last_updated > 2*HZ ) ||
+  if ((jiffies - data->last_updated > (data->type == adm9240?HZ/2:HZ*2) ) ||
       (jiffies < data->last_updated) || ! data->valid) {
 
 #ifdef DEBUG
