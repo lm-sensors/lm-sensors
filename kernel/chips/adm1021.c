129e08cc946ec7f7d5c5ec2af8e6c7c6c6cc3520
Mark D. Studebaker <mdsxyz123@yahoo.com>
1999-10-20 01:51:27 +0000

    (mds) Add support for LM84 (ADM1021 clone) to adm1021.c and sensors-detect.
    git-svn-id: http://lm-sensors.org/svn/lm-sensors/trunk@602 7894878c-1315-0410-8ee3-d5d059ff63e0

--- kernel/chips/adm1021.c
+++ kernel/chips/adm1021.c
@@ -42,7 +42,7 @@ static unsigned int normal_isa[] = {SENSORS_ISA_END};
 static unsigned int normal_isa_range[] = {SENSORS_ISA_END};
 
 /* Insmod parameters */
-SENSORS_INSMOD_4(adm1021,max1617,max1617a,thmc10);
+SENSORS_INSMOD_5(adm1021,max1617,max1617a,thmc10,lm84);
 
 /* adm1021 constants specified below */
 
@@ -248,6 +248,8 @@ static int adm1021_detect(struct i2c_adapter *adapter, int address,
       kind = adm1021;
     else if (i == 0x49)
       kind = thmc10;
+    else if (i == 0x00)
+      kind = lm84;
     else if ((i== 0x4d) && 
              (adm1021_read_value(new_client,ADM1021_REG_DEV_ID) == 0x01))
       kind = max1617a;
