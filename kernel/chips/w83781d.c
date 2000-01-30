0efe92ad9a0c32b0b3f87d3374c13fe9286f0574
Mark D. Studebaker <mdsxyz123@yahoo.com>
2000-01-30 02:37:03 +0000

    (mds) more parentheses in detection if statement - can't hurt.
    git-svn-id: http://lm-sensors.org/svn/lm-sensors/trunk@719 7894878c-1315-0410-8ee3-d5d059ff63e0

--- kernel/chips/w83781d.c
+++ kernel/chips/w83781d.c
@@ -729,12 +729,12 @@ int w83781d_detect(struct i2c_adapter *adapter, int address,
 		val1 = w83781d_read_value(new_client, W83781D_REG_BANK);
 		val2 = w83781d_read_value(new_client, W83781D_REG_CHIPMAN);
 		/* Check for Winbond or Asus ID if in bank 0 */
-		if (!(val1 & 0x07) &&
-		    ((!(val1 & 0x80) && (val2 != 0xa3) && (val2 != 0xc3))
+		if ((!(val1 & 0x07)) &&
+		    (((!(val1 & 0x80)) && (val2 != 0xa3) && (val2 != 0xc3))
 		     || ((val1 & 0x80) && (val2 != 0x5c)
 			 && (val2 != 0x12)))) goto ERROR1;
 		/* If Winbond SMBus, check address at 0x48. Asus doesn't support */
-		if ((!is_isa) && ((!(val1 & 0x80) && (val2 == 0xa3)) ||
+		if ((!is_isa) && (((!(val1 & 0x80)) && (val2 == 0xa3)) ||
 				  ((val1 & 0x80) && (val2 == 0x5c)))) {
 			if (w83781d_read_value
 			    (new_client, W83781D_REG_I2C_ADDR) != address)
