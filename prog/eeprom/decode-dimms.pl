ce6f623a4220cbdd39d0f4b486f77ad81772b574
Mark D. Studebaker <mdsxyz123@yahoo.com>
2000-09-21 02:27:21 +0000

    (mds) Add GPL boilerplate to comments.
    git-svn-id: http://lm-sensors.org/svn/lm-sensors/trunk@881 7894878c-1315-0410-8ee3-d5d059ff63e0

--- prog/eeprom/decode-dimms.pl
+++ prog/eeprom/decode-dimms.pl
@@ -3,6 +3,20 @@
 # Copyright 1998, 1999 Philip Edelbrock <phil@netroedge.com>
 # modified by Christian Zuckschwerdt <zany@triq.net>
 #
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
 # Version 0.4  1999  Philip Edelbrock <phil@netroedge.com>
 # Version 0.5  2000-03-30  Christian Zuckschwerdt <zany@triq.net>
 #  html output (selectable by commandline switches)
