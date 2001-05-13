3cbc85310038eef19df33f942c1d8c1a11db7f70
Mark D. Studebaker <mdsxyz123@yahoo.com>
2001-05-13 16:32:50 +0000

    Add support for maxilife-nba by just checking for "maxilife-" prefix.
    git-svn-id: http://lm-sensors.org/svn/lm-sensors/trunk@1085 7894878c-1315-0410-8ee3-d5d059ff63e0

--- prog/sensors/main.c
+++ prog/sensors/main.c
@@ -316,9 +316,7 @@ void do_a_print(sensors_chip_name name)
            (!strcmp(name.prefix,"w83627hf")) ||
            (!strcmp(name.prefix,"as99127f")))
     print_w83781d(&name);
-  else if (!strcmp(name.prefix,"maxilife-cg") ||
-           !strcmp(name.prefix,"maxilife-co") ||
-           !strcmp(name.prefix,"maxilife-as"))
+  else if (!strcmp(name.prefix,"maxilife-"))
     print_maxilife(&name);
   else if (!strcmp(name.prefix,"ddcmon"))
     print_ddcmon(&name);
