4f09f4f51c98feff47ce13085413b97ae5d39dfb
Martin Knoblauch <mkn@teraport.de>
2001-11-08 17:16:21 +0000

    (MKN) Add suppurt for FSC Scylla.
    git-svn-id: http://lm-sensors.org/svn/lm-sensors/trunk@1221 7894878c-1315-0410-8ee3-d5d059ff63e0

--- kernel/chips/Module.mk
+++ kernel/chips/Module.mk
@@ -55,6 +55,9 @@ endif
 ifneq ($(shell if grep -q '^CONFIG_SENSORS_FSCPOS=y' $(LINUX)/.config; then echo 1; fi),1)
 KERNELCHIPSTARGETS += $(MODULE_DIR)/fscpos.o
 endif
+ifneq ($(shell if grep -q '^CONFIG_SENSORS_FSCSCY=y' $(LINUX)/.config; then echo 1; fi),1)
+KERNELCHIPSTARGETS += $(MODULE_DIR)/fscscy.o
+endif
 ifneq ($(shell if grep -q '^CONFIG_SENSORS_GL518SM=y' $(LINUX)/.config; then echo 1; fi),1)
 KERNELCHIPSTARGETS += $(MODULE_DIR)/gl518sm.o
 endif
