406a7deeb8cf85de88e889ed4b17f4fac5368bab
Mark D. Studebaker <mdsxyz123@yahoo.com>
2002-07-02 03:08:02 +0000

    vt1211 update; move calculations from driver to sensors.conf
    git-svn-id: http://lm-sensors.org/svn/lm-sensors/trunk@1417 7894878c-1315-0410-8ee3-d5d059ff63e0

--- kernel/chips/vt1211.c
+++ kernel/chips/vt1211.c
@@ -153,7 +153,7 @@ static const u8 reghyst[] = { 0x3a, 0x3e, 0x1e, 0x2c, 0x2e, 0x30, 0x32 };
 #define VT1211_REG_FANDIV 0x47
 #define VT1211_REG_UCH_CONFIG 0x4a
 #define VT1211_REG_TEMP1_CONFIG 0x4b
-#define VT1211_REG_TEMP1_CONFIG 0x4c
+#define VT1211_REG_TEMP2_CONFIG 0x4c
 
 /* temps 1-7; voltages 0-6 */
 #define ISTEMP(i, ch_config) ((i) == 1 ? 1 : \
@@ -167,65 +167,13 @@ static const u8 reghyst[] = { 0x3a, 0x3e, 0x1e, 0x2c, 0x2e, 0x30, 0x32 };
 #define PWM_FROM_REG(val) (((val) * 1005) / 2550)
 #define PWM_TO_REG(val) SENSORS_LIMIT((((val) * 2555) / 1000), 0, 255)
 
-/* Conversions. Rounding and limit checking is only done on the TO_REG
-   variants. */
+#define TEMP_FROM_REG(val) ((val)*10)
+#define TEMP_FROM_REG10(val) (((val)*10)/4)
+#define TEMP_TO_REG(val) (SENSORS_LIMIT(((val)<0?(((val)-5)/10):\
+                                                 ((val)+5)/10),0,255))
+#define IN_FROM_REG(val) /*(((val)*10+5)/10)*/ (val)
+#define IN_TO_REG(val)  (SENSORS_LIMIT((((val) * 10 + 5)/10),0,255))
 
-/********* VOLTAGE CONVERSIONS (Bob Dougherty) ********/
-// From HWMon.cpp (Copyright 1998-2000 Jonathan Teh Soon Yew):
-// voltagefactor[0]=1.25/2628; (2628/1.25=2102.4)   // Vccp
-// voltagefactor[1]=1.25/2628; (2628/1.25=2102.4)   // +2.5V
-// voltagefactor[2]=1.67/2628; (2628/1.67=1573.7)   // +3.3V
-// voltagefactor[3]=2.6/2628;  (2628/2.60=1010.8)   // +5V
-// voltagefactor[4]=6.3/2628;  (2628/6.30=417.14)   // +12V
-// in[i]=(data[i+2]*25.0+133)*voltagefactor[i];
-// That is:
-// volts = (25*regVal+133)*factor
-// regVal = (volts/factor-133)/25
-// (These conversions were contributed by Jonathan Teh Soon Yew 
-// <j.teh@iname.com>)
-// 
-// These get us close, but they don't completely agree with what my BIOS 
-// says- they are all a bit low.  But, it all we have to go on...
-extern inline u8 IN_TO_REG(long val, int inNum)
-{
-	// to avoid floating point, we multiply everything by 100.
-	// val is guaranteed to be positive, so we can achieve the effect of 
-	// rounding by (...*10+5)/10.  Note that the *10 is hidden in the 
-	// /250 (which should really be /2500).
-	// At the end, we need to /100 because we *100 everything and we need
-	// to /10 because of the rounding thing, so we /1000.  
-	if (inNum <= 1)
-		return (u8)
-		    SENSORS_LIMIT(((val * 210240 - 13300) / 250 + 5) / 1000, 
-				  0, 255);
-	else if (inNum == 2)
-		return (u8)
-		    SENSORS_LIMIT(((val * 157370 - 13300) / 250 + 5) / 1000, 
-				  0, 255);
-	else if (inNum == 3)
-		return (u8)
-		    SENSORS_LIMIT(((val * 101080 - 13300) / 250 + 5) / 1000, 
-				  0, 255);
-	else
-		return (u8) SENSORS_LIMIT(((val * 41714 - 13300) / 250 + 5)
-					  / 1000, 0, 255);
-}
-
-extern inline long IN_FROM_REG(u8 val, int inNum)
-{
-	// to avoid floating point, we multiply everything by 100.
-	// val is guaranteed to be positive, so we can achieve the effect of
-	// rounding by adding 0.5.  Or, to avoid fp math, we do (...*10+5)/10.
-	// We need to scale with *100 anyway, so no need to /100 at the end.
-	if (inNum <= 1)
-		return (long) (((250000 * val + 13300) / 210240 * 10 + 5) /10);
-	else if (inNum == 2)
-		return (long) (((250000 * val + 13300) / 157370 * 10 + 5) /10);
-	else if (inNum == 3)
-		return (long) (((250000 * val + 13300) / 101080 * 10 + 5) /10);
-	else
-		return (long) (((250000 * val + 13300) / 41714 * 10 + 5) /10);
-}
 
 /********* FAN RPM CONVERSIONS ********/
 /* But this chip saturates back at 0, not at 255 like all the other chips.
@@ -235,156 +183,11 @@ extern inline u8 FAN_TO_REG(long rpm, int div)
 	if (rpm == 0)
 		return 0;
 	rpm = SENSORS_LIMIT(rpm, 1, 1000000);
-	return SENSORS_LIMIT((1350000 + rpm * div / 2) / (rpm * div), 1, 255);
+	return SENSORS_LIMIT((1310720 + rpm * div / 2) / (rpm * div), 1, 255);
 }
 
 #define MIN_TO_REG(a,b) FAN_TO_REG(a,b)
-#define FAN_FROM_REG(val,div) ((val)==0?0:(val)==255?0:1350000/((val)*(div)))
-
-/******** TEMP CONVERSIONS (Bob Dougherty) *********/
-// linear fits from HWMon.cpp (Copyright 1998-2000 Jonathan Teh Soon Yew)
-//      if(temp<169)
-//              return double(temp)*0.427-32.08;
-//      else if(temp>=169 && temp<=202)
-//              return double(temp)*0.582-58.16;
-//      else
-//              return double(temp)*0.924-127.33;
-//
-// A fifth-order polynomial fits the unofficial data (provided by Alex van 
-// Kaam <darkside@chello.nl>) a bit better.  It also give more reasonable 
-// numbers on my machine (ie. they agree with what my BIOS tells me).  
-// Here's the fifth-order fit to the 8-bit data:
-// temp = 1.625093e-10*val^5 - 1.001632e-07*val^4 + 2.457653e-05*val^3 - 
-//        2.967619e-03*val^2 + 2.175144e-01*val - 7.090067e+0.
-//
-// (2000-10-25- RFD: thanks to Uwe Andersen <uandersen@mayah.com> for 
-// finding my typos in this formula!)
-//
-// Alas, none of the elegant function-fit solutions will work because we 
-// aren't allowed to use floating point in the kernel and doing it with 
-// integers doesn't rpovide enough precision.  So we'll do boring old 
-// look-up table stuff.  The unofficial data (see below) have effectively 
-// 7-bit resolution (they are rounded to the nearest degree).  I'm assuming 
-// that the transfer function of the device is monotonic and smooth, so a 
-// smooth function fit to the data will allow us to get better precision.  
-// I used the 5th-order poly fit described above and solved for
-// VIA register values 0-255.  I *10 before rounding, so we get tenth-degree 
-// precision.  (I could have done all 1024 values for our 10-bit readings, 
-// but the function is very linear in the useful range (0-80 deg C), so 
-// we'll just use linear interpolation for 10-bit readings.)  So, tempLUT 
-// is the temp at via register values 0-255:
-static const long tempLUT[] =
-    { -709, -688, -667, -646, -627, -607, -589, -570, -553, -536, -519,
-	    -503, -487, -471, -456, -442, -428, -414, -400, -387, -375,
-	    -362, -350, -339, -327, -316, -305, -295, -285, -275, -265,
-	    -255, -246, -237, -229, -220, -212, -204, -196, -188, -180,
-	    -173, -166, -159, -152, -145, -139, -132, -126, -120, -114,
-	    -108, -102, -96, -91, -85, -80, -74, -69, -64, -59, -54, -49,
-	    -44, -39, -34, -29, -25, -20, -15, -11, -6, -2, 3, 7, 12, 16,
-	    20, 25, 29, 33, 37, 42, 46, 50, 54, 59, 63, 67, 71, 75, 79, 84,
-	    88, 92, 96, 100, 104, 109, 113, 117, 121, 125, 130, 134, 138,
-	    142, 146, 151, 155, 159, 163, 168, 172, 176, 181, 185, 189,
-	    193, 198, 202, 206, 211, 215, 219, 224, 228, 232, 237, 241,
-	    245, 250, 254, 259, 263, 267, 272, 276, 281, 285, 290, 294,
-	    299, 303, 307, 312, 316, 321, 325, 330, 334, 339, 344, 348,
-	    353, 357, 362, 366, 371, 376, 380, 385, 390, 395, 399, 404,
-	    409, 414, 419, 423, 428, 433, 438, 443, 449, 454, 459, 464,
-	    469, 475, 480, 486, 491, 497, 502, 508, 514, 520, 526, 532,
-	    538, 544, 551, 557, 564, 571, 578, 584, 592, 599, 606, 614,
-	    621, 629, 637, 645, 654, 662, 671, 680, 689, 698, 708, 718,
-	    728, 738, 749, 759, 770, 782, 793, 805, 818, 830, 843, 856,
-	    870, 883, 898, 912, 927, 943, 958, 975, 991, 1008, 1026, 1044,
-	    1062, 1081, 1101, 1121, 1141, 1162, 1184, 1206, 1229, 1252,
-	    1276, 1301, 1326, 1352, 1378, 1406, 1434, 1462
-};
-
-/* the original LUT values from Alex van Kaam <darkside@chello.nl> 
-   (for via register values 12-240):
-{-50,-49,-47,-45,-43,-41,-39,-38,-37,-35,-34,-33,-32,-31,
--30,-29,-28,-27,-26,-25,-24,-24,-23,-22,-21,-20,-20,-19,-18,-17,-17,-16,-15,
--15,-14,-14,-13,-12,-12,-11,-11,-10,-9,-9,-8,-8,-7,-7,-6,-6,-5,-5,-4,-4,-3,
--3,-2,-2,-1,-1,0,0,1,1,1,3,3,3,4,4,4,5,5,5,6,6,7,7,8,8,9,9,9,10,10,11,11,12,
-12,12,13,13,13,14,14,15,15,16,16,16,17,17,18,18,19,19,20,20,21,21,21,22,22,
-22,23,23,24,24,25,25,26,26,26,27,27,27,28,28,29,29,30,30,30,31,31,32,32,33,
-33,34,34,35,35,35,36,36,37,37,38,38,39,39,40,40,41,41,42,42,43,43,44,44,45,
-45,46,46,47,48,48,49,49,50,51,51,52,52,53,53,54,55,55,56,57,57,58,59,59,60,
-61,62,62,63,64,65,66,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,83,84,
-85,86,88,89,91,92,94,96,97,99,101,103,105,107,109,110};
-*/
-
-// Here's the reverse LUT.  I got it by doing a 6-th order poly fit (needed
-// an extra term for a good fit to these inverse data!) and then 
-// solving for each temp value from -50 to 110 (the useable range for 
-// this chip).  Here's the fit: 
-// viaRegVal = -1.160370e-10*val^6 +3.193693e-08*val^5 - 1.464447e-06*val^4 
-// - 2.525453e-04*val^3 + 1.424593e-02*val^2 + 2.148941e+00*val +7.275808e+01)
-// Note that n=161:
-static const u8 viaLUT[] =
-    { 12, 12, 13, 14, 14, 15, 16, 16, 17, 18, 18, 19, 20, 20, 21, 22, 23,
-	    23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 35, 36, 37, 39, 40,
-	    41, 43, 45, 46, 48, 49, 51, 53, 55, 57, 59, 60, 62, 64, 66,
-	    69, 71, 73, 75, 77, 79, 82, 84, 86, 88, 91, 93, 95, 98, 100,
-	    103, 105, 107, 110, 112, 115, 117, 119, 122, 124, 126, 129,
-	    131, 134, 136, 138, 140, 143, 145, 147, 150, 152, 154, 156,
-	    158, 160, 162, 164, 166, 168, 170, 172, 174, 176, 178, 180,
-	    182, 183, 185, 187, 188, 190, 192, 193, 195, 196, 198, 199,
-	    200, 202, 203, 205, 206, 207, 208, 209, 210, 211, 212, 213,
-	    214, 215, 216, 217, 218, 219, 220, 221, 222, 222, 223, 224,
-	    225, 226, 226, 227, 228, 228, 229, 230, 230, 231, 232, 232,
-	    233, 233, 234, 235, 235, 236, 236, 237, 237, 238, 238, 239,
-	    239, 240
-};
-
-/* Converting temps to (8-bit) hyst and over registers */
-// No interpolation here.  Just check the limits and go.
-// The +5 effectively rounds off properly and the +50 is because 
-// the temps start at -50
-extern inline u8 TEMP_TO_REG(long val)
-{
-	return (u8)
-	    SENSORS_LIMIT(viaLUT[((val <= -500) ? 0 : (val >= 1100) ? 160 : 
-				  ((val + 5) / 10 + 50))], 0, 255);
-}
-
-/* for 8-bit temperature hyst and over registers */
-// The temp values are already *10, so we don't need to do that.
-// But we _will_ round these off to the nearest degree with (...*10+5)/10
-#define TEMP_FROM_REG(val) ((tempLUT[(val)]*10+5)/10)
-
-/* for 10-bit temperature readings */
-// You might _think_ this is too long to inline, but's it's really only
-// called once...
-extern inline long TEMP_FROM_REG10(u16 val)
-{
-	// the temp values are already *10, so we don't need to do that.
-	long temp;
-	u16 eightBits = val >> 2;
-	u16 twoBits = val & 3;
-
-	// handle the extremes first (they won't interpolate well! ;-)
-	if (val == 0)
-		return (long) tempLUT[0];
-	if (val == 1023)
-		return (long) tempLUT[255];
-
-	if (twoBits == 0)
-		return (long) tempLUT[eightBits];
-	else {
-		// do some interpolation by multipying the lower and upper
-		// bounds by 25, 50 or 75, then /100.
-		temp = ((25 * (4 - twoBits)) * tempLUT[eightBits]
-			+ (25 * twoBits) * tempLUT[eightBits + 1]);
-		// increase the magnitude by 50 to achieve rounding.
-		if (temp > 0)
-			temp += 50;
-		else
-			temp -= 50;
-		return (temp / 100);
-	}
-}
-
-#define VT1211_INIT_FAN_MIN_1 3000
-#define VT1211_INIT_FAN_MIN_2 3000
+#define FAN_FROM_REG(val,div) ((val)==0?0:(val)==255?0:1310720/((val)*(div)))
 
 #ifdef MODULE
 extern int init_module(void);
@@ -689,6 +492,9 @@ void vt1211_init_client(struct i2c_client *client)
 	struct vt1211_data *data = client->data;
 
 	data->vrm = DEFAULT_VRM;
+	/* set "default" interrupt mode for alarms, which isn't the default */
+	vt1211_write_value(client, VT1211_REG_TEMP1_CONFIG, 0);
+	vt1211_write_value(client, VT1211_REG_TEMP2_CONFIG, 0);
 }
 
 void vt1211_update_client(struct i2c_client *client)
@@ -773,7 +579,6 @@ void vt1211_update_client(struct i2c_client *client)
 		data->fan_div[1] = i >> 6;
 		data->alarms = vt_rdval(client, VT1211_REG_ALARM1) |
 		                    (vt_rdval(client, VT1211_REG_ALARM2) << 8);
-		data->uch_config= vt_rdval(client, VT1211_REG_UCH_CONFIG);
 		data->vid= vt_rdval(client, VT1211_REG_VID) & 0x1f;
 		data->last_updated = jiffies;
 		data->valid = 1;
@@ -793,18 +598,18 @@ void vt1211_in(struct i2c_client *client, int operation, int ctl_name,
 		*nrels_mag = 2;
 	else if (operation == SENSORS_PROC_REAL_READ) {
 		vt1211_update_client(client);
-		results[0] = IN_FROM_REG(data->in_min[nr], nr);
-		results[1] = IN_FROM_REG(data->in_max[nr], nr);
-		results[2] = IN_FROM_REG(data->in[nr], nr);
+		results[0] = IN_FROM_REG(data->in_min[nr]);
+		results[1] = IN_FROM_REG(data->in_max[nr]);
+		results[2] = IN_FROM_REG(data->in[nr]);
 		*nrels_mag = 3;
 	} else if (operation == SENSORS_PROC_REAL_WRITE) {
 		if (*nrels_mag >= 1) {
-			data->in_min[nr] = IN_TO_REG(results[0], nr);
+			data->in_min[nr] = IN_TO_REG(results[0]);
 			vt1211_write_value(client, VT1211_REG_IN_MIN(nr),
 					    data->in_min[nr]);
 		}
 		if (*nrels_mag >= 2) {
-			data->in_max[nr] = IN_TO_REG(results[1], nr);
+			data->in_max[nr] = IN_TO_REG(results[1]);
 			vt1211_write_value(client, VT1211_REG_IN_MAX(nr),
 					    data->in_max[nr]);
 		}
@@ -968,13 +773,16 @@ void vt1211_uch(struct i2c_client *client, int operation, int ctl_name,
 {
 	struct vt1211_data *data = client->data;
 	if (operation == SENSORS_PROC_REAL_INFO)
-		*nrels_mag = 1;
+		*nrels_mag = 0;
 	else if (operation == SENSORS_PROC_REAL_READ) {
 		results[0] = data->uch_config;
 		*nrels_mag = 1;
 	} else if (operation == SENSORS_PROC_REAL_WRITE) {
-		if (*nrels_mag >= 1)
-			data->vrm = results[0];
+		if (*nrels_mag >= 1) {
+			data->uch_config = results[0] & 0x7c;
+			vt1211_write_value(client, VT1211_REG_UCH_CONFIG,
+			                   results[0] & 0x7c);
+		}
 	}
 }
 
