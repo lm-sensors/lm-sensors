d1da546a0964e9b4d31a6efbee7b64706a1416f8
Mark D. Studebaker <mdsxyz123@yahoo.com>
2002-10-12 00:56:49 +0000

    add support for 8233A PCI ID 0x3177. Tested by user in ticket #1017.
    git-svn-id: http://lm-sensors.org/svn/lm-sensors/trunk@1558 7894878c-1315-0410-8ee3-d5d059ff63e0

--- kernel/busses/i2c-viapro.c
+++ kernel/busses/i2c-viapro.c
@@ -25,8 +25,9 @@
 	82C596A/B (0x3050)
 	82C596B (0x3051)
 	82C686A/B
-	8233, 8233A
 	8231
+	8233
+	8233A (0x3147 and 0x3177)
    Note: we assume there can only be one device, with one SMBus interface.
 */
 
@@ -72,6 +73,7 @@ static struct sd supported[] = {
 	{PCI_DEVICE_ID_VIA_82C686_4, SMBBA1, 0xD2, "VT82C686A/B"},
 	{PCI_DEVICE_ID_VIA_8233_0, SMBBA3, 0xD2, "VT8233"},
 	{0x3147, SMBBA3, 0xD2, "VT8233A"},
+	{0x3177, SMBBA3, 0xD2, "VT8233A"},
 	{0x8235, SMBBA1, 0xD2, "VT8231"},
 	{0, 0, 0, NULL}
 };
