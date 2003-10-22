aa42ce8ce7e1039ed16537c53a2e1171270d8980
Mark M. Hoffman <mhoffman@lightlink.com>
2003-10-22 02:07:30 +0000

    Add some missing glue for w83627thf support in libsensors.
    git-svn-id: http://lm-sensors.org/svn/lm-sensors/trunk@2020 7894878c-1315-0410-8ee3-d5d059ff63e0

--- lib/chips.c
+++ lib/chips.c
@@ -4620,6 +4620,8 @@ sensors_chip_features sensors_chip_features_list[] =
  { SENSORS_W83697HF_PREFIX, w83782d_features }, /* same as 782d */
 		/* Cheat on 83627HF for now - no separate #defines */
  { SENSORS_W83627HF_PREFIX, w83782d_features },
+		/* Cheat on 83627THF for now - no separate #defines */
+ { SENSORS_W83627THF_PREFIX, w83782d_features },
  { SENSORS_W83791D_PREFIX, w83791d_features },
  { SENSORS_AS99127F_PREFIX, as99127f_features },
  { SENSORS_ADM9240_PREFIX, adm9240_features },
