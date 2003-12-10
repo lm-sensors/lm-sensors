e8c33a6575e7a63b79270c41dfe983303f9bd1fc
Mark M. Hoffman <mhoffman@lightlink.com>
2003-12-10 05:36:24 +0000

    Fix comment.
    git-svn-id: http://lm-sensors.org/svn/lm-sensors/trunk@2152 7894878c-1315-0410-8ee3-d5d059ff63e0

--- kernel/include/sensors_vid.h
+++ kernel/include/sensors_vid.h
@@ -1,5 +1,5 @@
 /*
-    vrm.c - Part of lm_sensors, Linux kernel modules for hardware
+    sensors_vid.h - Part of lm_sensors, Linux kernel modules for hardware
                monitoring
     Copyright (c) 2002 Mark D. Studebaker <mdsxyz123@yahoo.com>
     With assistance from Trent Piepho <xyzzy@speakeasy.org>
