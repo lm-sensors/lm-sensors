520da351726fcdf0e541ba9c4585b8b8c55c5c0c
Mark D. Studebaker <mdsxyz123@yahoo.com>
2004-02-28 16:34:08 +0000

    fix comment
    git-svn-id: http://lm-sensors.org/svn/lm-sensors/trunk@2319 7894878c-1315-0410-8ee3-d5d059ff63e0

--- kernel/chips/vt8231.c
+++ kernel/chips/vt8231.c
@@ -19,7 +19,7 @@
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
-/* Supports VIA VT8231 Super I/O embedded sensors */
+/* Supports VIA VT8231 South Bridge embedded sensors */
 
 #include <linux/module.h>
 #include <linux/slab.h>
