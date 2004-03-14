664c5caf86a2d82640c0bf272cda08bdb070b45b
Jean Delvare <khali@linux-fr.org>
2004-03-14 20:32:44 +0000

    Remove reference to LM78 in ds1621.
    git-svn-id: http://lm-sensors.org/svn/lm-sensors/trunk@2357 7894878c-1315-0410-8ee3-d5d059ff63e0

--- prog/sensors/chips.c
+++ prog/sensors/chips.c
@@ -125,7 +125,7 @@ void print_ds1621(const sensors_chip_name *name)
   double cur,hyst,over;
   int alarms, valid;
 
-  if (!sensors_get_feature(*name,SENSORS_LM78_ALARMS,&cur)) 
+  if (!sensors_get_feature(*name,SENSORS_DS1621_ALARMS,&cur)) 
     alarms = cur + 0.5;
   else {
     printf("ERROR: Can't get alarm data!\n");
