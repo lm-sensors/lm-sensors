c33a248478d5c6c62ed870275a9d11b4f693f04b
Jean Delvare <khali@linux-fr.org>
2006-06-23 13:45:08 +0000

    i2c-piix4: Add ATI IXP200, IXP300, IXP400 support (2.6 backport)
    git-svn-id: http://lm-sensors.org/svn/lm-sensors/trunk@4050 7894878c-1315-0410-8ee3-d5d059ff63e0

--- kernel/busses/i2c-piix4.c
+++ kernel/busses/i2c-piix4.c
@@ -23,6 +23,7 @@
    Supports:
 	Intel PIIX4, 440MX
 	Serverworks OSB4, CSB5, CSB6, HT-1000
+	ATI IXP200, IXP300, IXP400
 	SMSC Victory66
 
    Note: we assume there can only be one device, with one SMBus interface.
@@ -455,6 +456,10 @@ static struct i2c_adapter piix4_adapter = {
 	.dec_use	= piix4_dec,
 };
 
+#define PCI_DEVICE_ID_ATI_IXP200_SMBUS	0x4353
+#define PCI_DEVICE_ID_ATI_IXP300_SMBUS	0x4363
+#define PCI_DEVICE_ID_ATI_IXP400_SMBUS	0x4372
+
 #ifndef PCI_DEVICE_ID_SERVERWORKS_CSB6
 #define PCI_DEVICE_ID_SERVERWORKS_CSB6 0x0203
 #endif
@@ -468,6 +473,27 @@ static struct pci_device_id piix4_ids[] __devinitdata = {
 		.subvendor =	PCI_ANY_ID,
 		.subdevice =	PCI_ANY_ID,
 		.driver_data =	3
+	},
+	{
+		.vendor =	PCI_VENDOR_ID_ATI,
+		.device =	PCI_DEVICE_ID_ATI_IXP200_SMBUS,
+		.subvendor =	PCI_ANY_ID,
+		.subdevice =	PCI_ANY_ID,
+		.driver_data =	0,
+	},
+	{
+		.vendor =	PCI_VENDOR_ID_ATI,
+		.device =	PCI_DEVICE_ID_ATI_IXP300_SMBUS,
+		.subvendor =	PCI_ANY_ID,
+		.subdevice =	PCI_ANY_ID,
+		.driver_data =	0,
+	},
+	{
+		.vendor =	PCI_VENDOR_ID_ATI,
+		.device =	PCI_DEVICE_ID_ATI_IXP400_SMBUS,
+		.subvendor =	PCI_ANY_ID,
+		.subdevice =	PCI_ANY_ID,
+		.driver_data =	0,
 	},
 	{
 		.vendor =	PCI_VENDOR_ID_SERVERWORKS,
