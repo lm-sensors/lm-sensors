bf96a279b8aa3357293078bfab269c8c9dbdabf4
Jean Delvare <khali@linux-fr.org>
2006-10-08 15:45:15 +0000

    libsensors: Probe for busses before chips. This makes more sense that way, and may allow for cleanups later.
    git-svn-id: http://lm-sensors.org/svn/lm-sensors/trunk@4198 7894878c-1315-0410-8ee3-d5d059ff63e0

--- lib/init.c
+++ lib/init.c
@@ -43,10 +43,10 @@ int sensors_init(FILE *input)
   int res;
   sensors_cleanup();
   if (sensors_init_sysfs()) {
-    if ((res = sensors_read_sysfs_chips()) || (res = sensors_read_sysfs_bus()))
+    if ((res = sensors_read_sysfs_bus()) || (res = sensors_read_sysfs_chips()))
       return res;
   } else {
-    if ((res = sensors_read_proc_chips()) || (res = sensors_read_proc_bus()))
+    if ((res = sensors_read_proc_bus()) || (res = sensors_read_proc_chips()))
       return res;
   }
   if ((res = sensors_scanner_init(input)))
