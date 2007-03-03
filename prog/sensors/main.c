49a1b7a5030c4c86f06a50af3fefcd1ed03febfa
Jean Delvare <khali@linux-fr.org>
2007-03-03 14:10:39 +0000

    sensors: Do not print adapter errors when -A is used.
    git-svn-id: http://lm-sensors.org/svn/lm-sensors/trunk@4334 7894878c-1315-0410-8ee3-d5d059ff63e0

--- prog/sensors/main.c
+++ prog/sensors/main.c
@@ -428,7 +428,6 @@ struct match matches[] = {
 
 void do_a_print(sensors_chip_name name)
 {
-  const char *adap;
   struct match *m;
 
   /* do we know how to display it? */
@@ -440,11 +439,13 @@ void do_a_print(sensors_chip_name name)
     return;
 
   printf("%s\n",sprintf_chip_name(name));
-  adap = sensors_get_adapter_name(name.bus);
-  if (adap && !hide_adapter)
-    printf("Adapter: %s\n",adap);
-  if (!adap)
-    fprintf(stderr, "Can't get adapter name for bus %d\n", name.bus);
+  if (!hide_adapter) {
+    const char *adap = sensors_get_adapter_name(name.bus);
+    if (adap)
+      printf("Adapter: %s\n", adap);
+    else
+      fprintf(stderr, "Can't get adapter name for bus %d\n", name.bus);
+  }
   if (do_unknown)
     print_unknown_chip(&name);
   else {
