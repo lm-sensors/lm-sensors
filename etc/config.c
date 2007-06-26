2b1fb387e9bca5a5ee799de92fce4188bd873aec
Jean Delvare <khali@linux-fr.org>
2007-06-26 13:54:26 +0000

    Delete unused file.
    git-svn-id: http://lm-sensors.org/svn/lm-sensors/branches/lm-sensors-3.0.0@4476 7894878c-1315-0410-8ee3-d5d059ff63e0

--- etc/config.c
+++
@@ -1,8 +0,0 @@
-/*
- * This is used by the top-level makefile to automagically
- * get the kernel version from <linux/version.h>.
- * A simple grep doesn't work for Mandrake and Red Hat distributions
- * that contain multiple UTS_RELEASE definitions in version.h.
- */
-#include <linux/version.h>
-char *uts_release=UTS_RELEASE;
