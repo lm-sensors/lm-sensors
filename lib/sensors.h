a692aed7052511458097880ea45c7b2120985f52
Jean Delvare <khali@linux-fr.org>
2007-08-18 06:56:06 +0000

    Fix a broken comment.
    git-svn-id: http://lm-sensors.org/svn/lm-sensors/trunk@4683 7894878c-1315-0410-8ee3-d5d059ff63e0

--- lib/sensors.h
+++ lib/sensors.h
@@ -78,8 +78,8 @@ extern const char *sensors_get_algorithm_name(int bus_nr);
 
 /* Look up the label which belongs to this chip. Note that chip should not
    contain wildcard values! *result is newly allocated (free it yourself).
-   This function will return 0 on success, and <0 on failure.  This
-   function takes logical mappings into account. */
+   This function will return 0 on success, and <0 on failure.
+   If no label exists for this feature, its name is returned itself. */
 extern int sensors_get_label(sensors_chip_name name, int feature,
                              char **result);
 
