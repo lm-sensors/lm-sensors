lm-sensors/lm-sensors

commit eb62f8fa7bdbacf3f0e16eaad157de0c57392787

Fix lm93 VID support for Linux 2.6.
git-svn-id: http://lm-sensors.org/svn/lm-sensors/trunk@4745 7894878c-1315-0410-8ee3-d5d059ff63e0

lib/chips.c
@@ -1193,11 +1193,6 @@ static sensors_chip_feature as99127f_features[] =
 		SENSORS_LM93_TEMP##nr, SENSORS_LM93_TEMP##nr, RW }, \
 		LM93_SYSCTL_TEMP##nr, VALUE(2), 1 }
 
-/* macro for LM93 VID entries */
-#define SENSORS_LM93_VID(nr) \
-	{ { SENSORS_LM93_VID##nr, "vid" #nr, NOMAP, NOMAP, R }, \
-		 LM93_SYSCTL_VID##nr, VALUE(1), 3 }
-
 static sensors_chip_feature lm93_features[] = {
 	SENSORS_LM93_IN_MIN_MAX(1),
 	SENSORS_LM93_IN_MIN_MAX(2),
@@ -1222,8 +1217,10 @@ static sensors_chip_feature lm93_features[] = {
 	SENSORS_LM93_TEMP_MIN_MAX(1),
 	SENSORS_LM93_TEMP_MIN_MAX(2),
 	SENSORS_LM93_TEMP_MIN_MAX(3),
-	SENSORS_LM93_VID(1),
-	SENSORS_LM93_VID(2),
+    { { SENSORS_LM93_VID1, "vid1", NOMAP, NOMAP, R },
+                          LM93_SYSCTL_VID1, VALUE(1), 3, "cpu0_vid", 3, "vid1" },
+    { { SENSORS_LM93_VID1, "vid2", NOMAP, NOMAP, R },
+                          LM93_SYSCTL_VID2, VALUE(1), 3, "cpu1_vid", 3, "vid2" },
     { { SENSORS_LM93_ALARMS, "alarms", NOMAP, NOMAP, R }, 
                           LM93_SYSCTL_ALARMS, VALUE(1), 0 },
     { { 0 }, 0 }

commit 8b6882e8d6ff6c5d9d140cdece0c50f816235122

Fix lm93 vid2 access (#2295). It got broken by r4745.
git-svn-id: http://lm-sensors.org/svn/lm-sensors/trunk@5105 7894878c-1315-0410-8ee3-d5d059ff63e0

lib/chips.c
@@ -1219,7 +1219,7 @@ static sensors_chip_feature lm93_features[] = {
 	SENSORS_LM93_TEMP_MIN_MAX(3),
     { { SENSORS_LM93_VID1, "vid1", NOMAP, NOMAP, R },
                           LM93_SYSCTL_VID1, VALUE(1), 3, "cpu0_vid", 3, "vid1" },
-    { { SENSORS_LM93_VID1, "vid2", NOMAP, NOMAP, R },
+    { { SENSORS_LM93_VID2, "vid2", NOMAP, NOMAP, R },
                           LM93_SYSCTL_VID2, VALUE(1), 3, "cpu1_vid", 3, "vid2" },
     { { SENSORS_LM93_ALARMS, "alarms", NOMAP, NOMAP, R }, 
                           LM93_SYSCTL_ALARMS, VALUE(1), 0 },