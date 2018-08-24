cb0348a35f005c52dd8846b106dc85c24366d978
su8 <wifiextender2@gmail.com>
2018-08-24 11:33:06 +0200

    Move subCnt++; up to skip empty line with comma

--- prog/sensors/chips.c
+++ prog/sensors/chips.c
@@ -105,12 +105,12 @@ void print_chip_json(const sensors_chip_name *name)
 					if (subCnt > 0)
 						printf(",\n");
 					printf("         \"%s\": %.3f", sub->name, val);
+					subCnt++;
 				}
 
 			} else {
 				printf("(%s)", label);
 			}
-			subCnt++;
 		}
 		free(label);
 		printf("\n      }");
